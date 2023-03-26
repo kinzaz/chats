ff2cb30ec0d333fa590547f5de04003558675fe9
Vladislav Vlasenko <kinzazdev579@gmail.com>
2023-03-26 12:52:11 +0300

    websocket

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import EventSourcing from './EventSourcing';
 import LongPulling from './LongPulling';
+import WebSock from './WebSocket';
 
 function App() {
 	return (
 		<div className="App">
-			<EventSourcing />
+			<WebSock />
 		</div>
 	);
 }
